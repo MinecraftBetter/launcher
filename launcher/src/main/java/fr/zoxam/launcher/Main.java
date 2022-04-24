c07b88e03df95e00c849b1a8f8747ce7e5d89095
MazeWave <louisdalmasso2003@gmail.com>
2022-04-25 01:15:36 +0200

    Updated Main.java :wrench:
    C'est réel je corrige des fautes dans du code donc je suis un dév :+1:

--- launcher/src/main/java/fr/zoxam/launcher/Main.java
+++ launcher/src/main/java/fr/zoxam/launcher/Main.java
@@ -19,7 +19,7 @@ public class Main {
             Application.launch(FxApplication.class, args);
         } catch (ClassNotFoundException e) {
             logger.warn("JavaFX not found ");
-            JOptionPane.showMessageDialog(null, "Une erreur avec java à été détectée.\n" + e.getMessage(), "Erreur Jave", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Une erreur avec java à été détectée.\n" + e.getMessage(), "Erreur Java", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
