767c69a914b127cb04ecd9b51bff7221b8c9685c
EvanG <evan_g@orange.fr>
2022-06-02 14:06:24 +0200

    Log error causes

--- launcher/src/main/java/fr/minecraftbetter/launcher/utils/logging/LogFormatter.java
+++ launcher/src/main/java/fr/minecraftbetter/launcher/utils/logging/LogFormatter.java
@@ -1,24 +1,16 @@
 package fr.minecraftbetter.launcher.utils.logging;
 
+import java.text.MessageFormat;
 import java.util.Date;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
 
 public class LogFormatter extends SimpleFormatter {
-    private String format = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
-    private String formatStackTrace = "[%1$tF %1$tT] [%2$-7s] %3$s, %4$s %n";
-
-    public LogFormatter() {}
-
-    public LogFormatter(String format, String formatStackTrace) {
-        this.format = format;
-        this.formatStackTrace = format;
-    }
-
+    private static final String FORMAT = "[%1$tF %1$tT] [%2$-7s] %3$s%4$s %n";
 
     @Override
     public synchronized String format(LogRecord lr) {
-        return String.format(lr.getThrown() == null ? format : formatStackTrace,
-                new Date(lr.getMillis()), lr.getLevel().getLocalizedName(), lr.getMessage(), lr.getThrown());
+        String trace = lr.getThrown() == null ? "" : MessageFormat.format(", {0} ({1})", lr.getThrown().getMessage(), lr.getThrown().getCause());
+        return String.format(FORMAT, new Date(lr.getMillis()), lr.getLevel().getLocalizedName(), lr.getMessage(), trace);
     }
 }
